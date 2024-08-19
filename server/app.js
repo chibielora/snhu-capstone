32e2ac7c6dcf2df75d2fd59b6031c4f46d1fa5dc
chibielora <chibi.elora@gmail.com>
2024-08-18 23:24:51 -0400

    Log server errors

--- server/app.js
+++ server/app.js
@@ -45,3 +45,7 @@ app.use(function (err, req, res, next) {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+process.on("uncaughtException", e => {
+  console.error(e);
+})
